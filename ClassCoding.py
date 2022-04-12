68057716bd0333c3b8134f87ba21d3504239cf43
JakeThompson <JThompson@dev-10.com>
2022-04-12 11:23:20 -0500

    Expand ClassCoding.py to use inheritance

--- ClassCoding.py
+++ ClassCoding.py
@@ -14,7 +14,7 @@ class Animal:
     def sleep(self):
         print(f'{self.__species} is sleeping')
 
-class Books:
+class Book:
     """Books class"""
     def __init__(self, title, author, genre):
         self.__author = author
@@ -38,19 +38,57 @@ class Vehicle:
         self.__year = year
 
     def accelerate(self):
-        print("Vroom vroom")
+        print("*zoom*")
 
     def brake(self):
         print("skrrrrrrrt")
 
+class Fish(Animal):
+    def __init__(self,genus,species,color):
+        super().__init__(genus, species, color)
+        self.__species = species        #Example of using attribute of superclass
+
+    def move(self):                     #Example of polymorphism, overwriting parent's move()
+        print(f"{self.__species} is swimming")
+
+class Snake(Animal):
+    def shed(self):
+        print("Snake has shed")
+
+class Person(Animal):
+    def write(self):
+        print("Look ma, I have opposable thumbs!")
+
+class Textbook(Book):
+    def __init__(self, title, author, genre, subject):
+        super().__init__(title, author, genre)
+        self.__subject = subject
+
+class AddressBook(Book):
+    def __init__(self, title, author, genre, region):
+        super().__init__(title, author, genre, region)
+        self.__region = region
+
+class Car(Vehicle):
+    def honk():
+        print("Honk honk")
+
     def blinker(self):
         print("tick tock")
 
+class Bicycle(Vehicle):
+    def popWheelie():
+        print("Totally radical bro")
 
-snake = Animal('Thamnophis', 'couchii', 'black')
-snake.move()
-snake.eat()
+class Boat(Vehicle):
+    def __init__(self, make, model, year, boatClass):
+        super().__init__(make, model, year)
+        self.__boatClass = boatClass
 
-myCar = Vehicle("Nissan", "Altima", "some year")
-myCar.accelerate()
-myCar.brake()
+class HotAirBalloon(Vehicle):
+    def vent():
+        print("We're descending")
+
+
+fishie = Fish('aGenus','someFish','someColor')
+fishie.move()           #Example of referencing superclass attribute and polymorphism
